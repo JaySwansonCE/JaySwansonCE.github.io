f661dc3dd15cfd23ab8df891a376b4fb8a405f92
Jay Swanson <jay.swanson@s86533.local>
2020-08-24 11:52:30 -0400

    fixed skills/langauges

--- src/components/skills/text.js
+++ src/components/skills/text.js
@@ -1,5 +1,10 @@
 export const skills = [
   {
+    id: 0,
+    name: 'Pro/E',
+    value: 65,
+  },
+  {
     id: 1,
     name: 'AWS',
     value: 30,
@@ -21,19 +26,34 @@ export const skills = [
   },
   {
     id: 5,
-    name: 'AWS',
+    name: 'React',
     value: 75,
   },
   ,
   {
     id: 6,
-    name: 'Javascript',
-    value: 75,
+    name: 'Google Apps Suite',
+    value: 85,
   },
   {
     id: 7,
-    name: 'd3.js',
+    name: 'MS Office',
+    value: 80,
+  },
+  {
+    id: 8,
+    name: 'Solid Edge',
+    value: 65,
+  },
+  {
+    id: 9,
+    name: 'Pandas, Scikit-learn',
     value: 50,
+  },
+  {
+    id: 10,
+    name: 'AutoCAD',
+    value: 60,
   },
 ]
 
@@ -61,23 +81,28 @@ export const languages = [
   {
     id: 5,
     name: 'node.js',
-    value: 20,
+    value: 40,
   },
   ,
   {
     id: 6,
-    name: 'Javascript',
-    value: 75,
+    name: 'Verilog',
+    value: 35,
   },
   {
     id: 7,
-    name: 'd3.js',
+    name: 'MATLAB',
     value: 50,
+  },
+  {
+    id: 8,
+    name: 'BASH scripting',
+    value: 90,
   },
 ]
 
 export const intro =
-  'Aspiring Full-Stack Engineer. Done a lot of webscraping blah blah...'
+  'I started programming in C++ and moved to python and lots of MATLAB for my computer vision education. I have been exploring more python packages like pandas and scikit-learn for data reasons. I use JavaScript with React to build websites to display my projects.'
 
 export const intro2 =
-  'Another intro'
+  ''
