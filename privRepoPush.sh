c3d77727d3e508df5ba1995b736f698b305c9d52
swanjson <swanjson@gmail.com>
2020-08-27 14:04:52 -0400

    old school

---
+++ privRepoPush.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+setup_git() {
+  # git config --global user.name ${TRAVIS_LOGIN}
+  # git config --global user.name ${TRAVIS_PASS}
+  https://${TRAVIS_LOGIN}:${TRAVIS_PASS}@github.com/${TRAVIS_LOGIN}/resumes.git
+  git submodule add https://github.com/swanjson/resumes
+}
+
+# commit_website_files() {
+#   git checkout -b gh-pages
+#   git add . *.html
+#   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+# }
+
+# upload_files() {
+#   git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+#   git push --quiet --set-upstream origin-pages gh-pages 
+# }
+
+setup_git
+# commit_website_files
+# upload_files
